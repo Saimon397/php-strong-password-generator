d10f7841badbe332042408013856e1a7e306353e
Saimon397 <simonecarrabba@gmail.com>
2022-11-29 23:34:48 +0100

    add_style

--- index.php
+++ index.php
@@ -30,15 +30,19 @@ if (isset($_POST['numcar']) && !empty($_POST['numcar'])) {
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link href='https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css' rel='stylesheet' integrity='sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi' crossorigin='anonymous'>
     <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css' integrity='sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==' crossorigin='anonymous' referrerpolicy='no-referrer' />
+    <link rel="stylesheet" href="./css/style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Source+Code+Pro:wght@400;700&display=swap" rel="stylesheet">
     <title>PHP Strong Password Generator</title>
 </head>
 
 <body>
     <form action="index.php" method="POST">
-        <div class="mt-5 container d-flex justify-content-center">
-            <label for="" class="form-label text-uppercase fs-1 fw-bold me-3">Password Generator:</label>
-            <input type="number" class="text-center border rounded" name="numcar" placeholder="Choose the length...">
-            <button type="submit" class="btn btn-dark ms-3">Send</button>
+        <div class="container d-flex justify-content-center card">
+            <label for="" class="form-label text-uppercase text-white fs-1 fw-bold">Password Generator:</label>
+            <input type="number" class="text-center border rounded mt-2" name="numcar" placeholder="Choose the length...">
+            <button type="submit" class="btn btn-light mt-4 mb-4"><i class="fa-brands fa-php fa-flip" style="--fa-animation-duration: 3s;"></i></button>
         </div>
     </form>
 </body>
